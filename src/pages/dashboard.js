04c85dca0b0c53d447f1b6e64f754da4930d0342
Phillip Hogan <phillip.hogan@elucidat.com>
2020-11-18 06:50:14 +0000

    feat: add identity login

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Router } from '@reach/router';
 import { navigate } from 'gatsby';
 import Layout from '../components/layout';
@@ -6,13 +6,19 @@ import Profile from '../components/profile';
 import RouteBase from '../components/route-base';
 import RouteSecret from '../components/route-secret';
 import RouteLogin from '../components/route-login';
+import IdentityModal from 'react-netlify-identity-widget';
+import 'react-netlify-identity-widget/styles.css';
 
 const Dashboard = ({ location }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       navigate('/dashboard/login', { replace: true });
     }
   }, [location.pathname]);
+
+  const showModal = () => setIsVisible(true);
 
   return (
     <Layout>
@@ -20,8 +26,12 @@ const Dashboard = ({ location }) => {
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin path="/dashboard/login" showModal={showModal} />
       </Router>
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setIsVisible(false)}
+      />
     </Layout>
   );
 };
